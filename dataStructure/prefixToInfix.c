8c9f29ab1752c01573f5e47e97f5d4e8ec0d7bc3
shikharr505_ <shikharchangotra01@gmail.com>
2025-03-07 20:15:32 +0530

    Update prefixToInfix.c

--- dataStructure/prefixToInfix.c
+++ dataStructure/prefixToInfix.c
@@ -1,37 +1,38 @@
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+#include<math.h>
 
-char *convertPrefixToinfix(char *exp)
+char* convertPrefixToInfix(char* exp)
 {
-    int size = strlen(exp), top = -1;
+    int size=strlen(exp),top=-1;
     char stack[size][100];
-    for (int i = size - 1; i >= 0; i--)
-    {
-        char ch = exp[i];
-        if (isalnum(ch))
+    for(int i=size-1;i>=0;i--){
+        char ch=exp[i];
+        if(isalnum(ch))
         {
-            char temp[] = {ch, '\0'};
-            strcpy(stack[++top], temp);
+            char temp[]={ch,'\0'};
+            strcpy(stack[++top],temp);
         }
-        else if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^')
+        else if(ch=='+'||ch=='-'||ch=='*'||ch=='/'||ch=='^')
         {
-            char *s1 = stack[top--];
-            char *s2 = stack[top--];
+            char *s1=stack[top--];
+            char *s2=stack[top--];
             char s3[100];
-            snprintf(s3, 100, "(%s%c%s)", s1, ch, s2);
-            strcpy(stack[++top], s3);
+            snprintf(s3,100,"(%s%c%s)",s2,ch,s1);
+            strcpy(stack[++top],s3);
         }
     }
-    char *s = stack[top];
+    char* s=(char*)malloc(strlen(stack[top])+1);
+    strcpy(s,stack[top]);
     return s;
 }
 
-int main()
-{
+int main (){
     char exp[100];
-    printf("Enter postfix expression: ");
-    fgets(exp, sizeof(exp), stdin);
-    printf("Infix form of Prefix form %s is %s", exp, convertPrefixToinfix(exp));
-    return 0;
+    printf("Enter the prefix expression: ");
+    gets(exp);
+    printf("Infix form of the expression is: %s\n",convertPrefixToInfix(exp));
+    return 0;
 }
